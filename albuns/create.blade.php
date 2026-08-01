4ed0b6ccb9e272d192671d1af68ecb710c941d2d
MisterG001 <gustavoyvomartinsa@gmail.com>
2026-08-01 18:21:58 -0300

    Update create.blade.php

--- albuns/create.blade.php
+++ albuns/create.blade.php
@@ -1,15 +1,20 @@
-<h1>Lista de Álbuns</h1>
+<h1>Cadastrar Álbum</h1>
 
-<a href="/albuns/create">Ir para Formulário</a>
-<table>
-    @foreach ($albuns as $album)
-        <tr>
-            <td>
-                <h3><?= $album->name ?></h3>
-            </td>
-            <td>
-                <p><?= $album->artist ?></p>
-            </td>
-        </tr>
-    @endforeach
-</table>
+<form action="{{ route('albuns.store') }}" method="POST">
+    @csrf
+    <div>
+        <label>Nome do Álbum:</label><br>
+        <input type="text" name="nome" value="{{ old('nome') }}" required>
+    </div>
+    <div>
+        <label>Nome do artista:</label><br>
+        <input type="text" name="artista" value="{{ old('artista') }}" required>
+    </div>
+    <div>
+        <label>Ano de Lançamento:</label>
+        <input type="text" name="ano_lancamento" value="{{ old('ano_lancamento') }}">
+    </div>
+    <br>
+    <button type="submit">Salvar</button>
+    <a href="{{ route('albuns.index') }}">Cancelar</a>
+</form>
